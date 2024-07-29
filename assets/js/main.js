8a48957dc1379b0b8bf3e16b39c2bc8f9c5f1dcb
Denis <gundenis1@gmail>
2024-07-29 13:52:55 +0300

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,12 +74,12 @@ const swiper1 = new Swiper(".swiper-bank", {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 1.5,
+      slidesPerView: 3,
       spaceBetween: 10,
     },
     // when window width is >= 480px
     480: {
-      slidesPerView: 1.5,
+      slidesPerView:3,
       spaceBetween: 20,
     },
     // when window width is >= 640px
